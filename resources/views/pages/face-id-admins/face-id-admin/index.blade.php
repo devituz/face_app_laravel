fd8703e1c5e4470f25d2dcb40371539d5e0a1fa6
Shohbozbek <shohbozbek@Shohbozbeks-iMac.local>
2025-04-02 12:25:00 +0500

    `Tahrir: "Face id Admin List" ni "Mobile Admins List" ga o'zgartirdim, ba'zi elementlarni olib tashladim va jadval ustunlarini o'zgartirdim.`

--- resources/views/pages/face-id-admins/face-id-admin/index.blade.php
+++ resources/views/pages/face-id-admins/face-id-admin/index.blade.php
@@ -15,15 +15,13 @@
                         <div class="header-body">
                             <div class="row align-items-center">
                                 <div class="col">
-
-                                    <!-- Pretitle -->
                                     <h6 class="header-pretitle">
                                         Overview
                                     </h6>
 
-                                    <!-- Title -->
+
                                     <h1 class="header-title text-truncate">
-                                        Face id Admin List
+                                       Mobile Admins List
                                     </h1>
 
                                 </div>
@@ -32,7 +30,8 @@
                                         Add Admin
                                     </a>
                                 </div>
-                            </div> <!-- / .row -->
+                            </div>
+
 
                         </div>
                     </div>
@@ -41,8 +40,6 @@
                         <div class="card-header">
                             <div class="row align-items-center">
                                 <div class="col">
-
-                                    <!-- Form -->
                                     <form>
                                         <div class="input-group input-group-flush input-group-merge input-group-reverse">
                                             <input class="form-control list-search" type="search" placeholder="Search">
@@ -54,8 +51,6 @@
 
                                 </div>
                                 <div class="col-auto me-n3">
-
-                                    <!-- Select -->
                                     <form>
                                         <select class="form-select form-select-sm form-control-flush" data-choices='{"searchEnabled": false}'>
                                             <option>5 per page</option>
@@ -65,7 +60,8 @@
                                     </form>
 
                                 </div>
-                            </div> <!-- / .row -->
+                            </div>
+
                         </div>
 
                         <div class="table-responsive">
@@ -79,22 +75,22 @@
                                         </div>
                                     </th>
                                     <th>
-                                        <a class="list-sort text-body-secondary" data-sort="item-name">Fullname</a>
+                                        <a class="list-sort text-body-secondary" >Image</a>
                                     </th>
                                     <th>
-                                        <a class="list-sort text-body-secondary" data-sort="item-email">Email</a>
+                                        <a class="list-sort text-body-secondary" >Fullname</a>
                                     </th>
                                     <th>
-                                        <a class="list-sort text-body-secondary" data-sort="item-phone">Phone</a>
+                                        <a class="list-sort text-body-secondary" >Email</a>
                                     </th>
-                                    {{--                                    <th>--}}
-                                    {{--                                        <a class="list-sort text-body-secondary" data-sort="item-phone">Status</a>--}}
-                                    {{--                                    </th>--}}
                                     <th>
-                                        <a class="list-sort text-body-secondary" data-sort="item-password">Password</a>
+                                        <a class="list-sort text-body-secondary" >Phone</a>
+                                    </th>
+                                    <th>
+                                        <a class="list-sort text-body-secondary">Password</a>
                                     </th>
                                     <th colspan="2">
-                                        <a class="list-sort text-body-secondary" data-sort="item-company">Created At</a>
+                                        <a class="list-sort text-body-secondary" >Created At</a>
                                     </th>
                                 </tr>
                                 </thead>
@@ -102,27 +98,29 @@
                                 @foreach($admins as $admin)
                                     <tr>
                                         <td>
-                                            <!-- Checkbox -->
+
                                             <div class="form-check">
                                                 <input class="form-check-input list-checkbox" id="listCheckboxOne" type="checkbox" data-id="{{ $admin->id }}">
                                                 <label class="form-check-label" for="listCheckboxOne"></label>
                                             </div>
                                         </td>
                                         <td>
-                                            <div class="avatar avatar-xs align-middle me-2">
-                                                <img class="avatar-img rounded-circle" src="{{ $admin->image_url }}" alt="...">
+
+                                            <div class="avatar avatar-xs align-middle me-2" data-bs-toggle="modal" data-bs-target="#imageModal" data-image="{{ $admin->image_url }}">
+                                                <img class="avatar-img" src="{{ $admin->image_url }}" alt="...">
                                             </div>
-                                            <span class="item-title">{{ $admin->name }}</span>
                                         </td>
+
+                                         <td>
+                                            <span class="item-title">{{ $admin->name }}</span>
+                                         </td>
                                         <td>
                                             <span class="item-title">{{ $admin->email }}</span>
                                         </td>
                                         <td>
                                             <span class="item-phone text-reset">{{ $admin->phone }}</span>
                                         </td>
-                                        {{--                                        <td>--}}
-                                        {{--                                            <span class="item-score badge text-bg-danger-subtle">{{ $admin->is_admin == 1 ? 'Active' : 'Inactive' }}</span>--}}
-                                        {{--                                        </td>--}}
+
                                         <td>
                                             <span class="item-phone text-reset">{{ $admin->password }}</span>
                                         </td>
@@ -130,16 +128,13 @@
                                             <span class="item-phone text-reset">{{ $admin->formatted_created_at }}</span>
                                         </td>
                                         <td class="text-end">
-                                            <!-- Dropdown -->
+
                                             <div class="dropdown">
                                                 <a class="dropdown-ellipses dropdown-toggle" href="crm-contacts.html#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                                     <i class="fe fe-more-vertical"></i>
                                                 </a>
                                                 <div class="dropdown-menu dropdown-menu-end">
                                                     <a data-bs-target="#modalUpdate" href="{{ route('face-id-admin.edit', $admin->id) }}" class="dropdown-item">Edit</a>
-                                                    <a href="#deleteModal" class="dropdown-item text-danger" data-bs-toggle="modal" data-bs-target="#deleteModal" onclick="event.preventDefault(); document.getElementById('deleteForm').action = '{{ route('face-id-admin.destroy', $admin->id) }}';">
-                                                        Delete
-                                                    </a>
                                                 </div>
                                             </div>
                                         </td>
@@ -157,7 +152,7 @@
                         </div>
 
                         <div class="card-footer d-flex justify-content-between">
-                            <!-- Pagination (prev) -->
+
                             <ul class="list-pagination-prev pagination pagination-tabs card-pagination">
                                 <li class="page-item">
                                     <a class="page-link ps-0 pe-4 border-end" href="crm-contacts.html#">
@@ -166,10 +161,8 @@
                                 </li>
                             </ul>
 
-                            <!-- Pagination -->
-                            <ul class="list-pagination pagination pagination-tabs card-pagination"></ul>
 
-                            <!-- Pagination (next) -->
+                            <ul class="list-pagination pagination pagination-tabs card-pagination"></ul>
                             <ul class="list-pagination-next pagination pagination-tabs card-pagination">
                                 <li class="page-item">
                                     <a class="page-link ps-4 pe-0 border-start" href="crm-contacts.html#">
@@ -178,9 +171,7 @@
                                 </li>
                             </ul>
 
-                            <!-- Alert -->
                             <div class="list-alert alert alert-dark alert-dismissible border fade" role="alert">
-                                <!-- Content -->
                                 <div class="row align-items-center">
                                     <div class="col">
                                         <!-- Checkbox -->
@@ -197,19 +188,26 @@
                                         </button>
                                     </div>
                                 </div>
-                                <!-- / .row -->
-                                <!-- Close -->
                                 <button type="button" class="list-alert-close btn-close" aria-label="Close"></button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <!-- / .row -->
         </div>
     </div>
 
 
+    <script>
+        document.querySelectorAll('.avatar').forEach(function(avatar) {
+            avatar.addEventListener('click', function() {
+                var imageUrl = avatar.getAttribute('data-image');
+                document.getElementById('modalImage').src = imageUrl;
+            });
+        });
+
+    </script>
+    @extends('components.model')
     @extends('pages.face-id-admins.face-id-admin.modal')
 
 @endsection
