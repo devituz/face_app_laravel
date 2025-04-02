8aa22d173c97a90fe2c7065dcd7c05a0e5e58925
Shohbozbek <shohbozbek@Shohbozbeks-iMac.local>
2025-04-02 14:07:52 +0500

    Candidate qidiruv yo'lini yangilandi.

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::middleware('auth')->group(function () {
     Route::post('candidate-list/bulk-delete', [CandidateListController::class, 'bulkDestroy'])->name('candidate-list.bulkDelete');
     Route::get('candidatelist/export', [CandidateListController::class, 'export'])->name('candidatelist.export');
 
-    Route::get('/search-candidates', [CandidateController::class, 'search'])->name('candidates.search');
+    Route::get('/candidates/search', [CandidateController::class, 'search'])->name('candidate.search');
 
 
     Route::resource('face-id-admin', FaceidContoller::class);
