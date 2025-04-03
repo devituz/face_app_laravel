745fac5e7d2f564562ca2a6b5a3ba0db608e650e
devituz <shohbozbek.uz24@gmail.com>
2025-04-03 13:32:45 +0500

    ded

--- resources/views/pages/candidates/candidate/index.blade.php
+++ resources/views/pages/candidates/candidate/index.blade.php
@@ -154,54 +154,50 @@
                             </table>
                         </div>
 
-                                                <div class="card-footer d-flex justify-content-between">
-                                                    <!-- Pagination (prev) -->
-                                                                                @if ($prevPage)
-                                                                                    <a class="btn btn-outline-primary" href="{{ route('candidate.index', ['page' => $prevPage]) }}">
-                                                                                        <i class="fe fe-arrow-left"></i> Prev
-                                                                                    </a>
-                                                                                @else
-                                                                                    <button class="btn btn-outline-secondary disabled">
-                                                                                        <i class="fe fe-arrow-left"></i> Prev
-                                                                                    </button>
-                                                                                @endif
-                                                    <!-- Pagination -->
-{{--                                                    <ul class="list-pagination pagination pagination-tabs card-pagination"></ul>--}}
+                        <div class="card-footer d-flex justify-content-between">
+                            <!-- Pagination (prev) -->
+                            @if (request('query') === null && $prevPage) <!-- Qidiruv bo'lmasa paginatsiya ko'rsatiladi -->
+                            <a class="btn btn-outline-primary" href="{{ route('candidate.index', ['page' => $prevPage]) }}">
+                                <i class="fe fe-arrow-left"></i> Prev
+                            </a>
+                            @elseif (request('query') === null)
+                                <button class="btn btn-outline-secondary disabled">
+                                    <i class="fe fe-arrow-left"></i> Prev
+                                </button>
+                            @endif
 
-                                                    <!-- Pagination (next) -->
-                                                    @if ($nextPage)
-                                                        <a class="btn btn-outline-primary" href="{{ route('candidate.index', ['page' => $nextPage]) }}">
-                                                            Next <i class="fe fe-arrow-right"></i>
-                                                        </a>
-                                                    @else
-                                                        <button class="btn btn-outline-secondary disabled">
-                                                            Next <i class="fe fe-arrow-right"></i>
-                                                        </button>
-                                                    @endif
+                            <!-- Pagination (next) -->
+                            @if (request('query') === null && $nextPage) <!-- Qidiruv bo'lmasa paginatsiya ko'rsatiladi -->
+                            <a class="btn btn-outline-primary" href="{{ route('candidate.index', ['page' => $nextPage]) }}">
+                                Next <i class="fe fe-arrow-right"></i>
+                            </a>
+                            @elseif (request('query') === null)
+                                <button class="btn btn-outline-secondary disabled">
+                                    Next <i class="fe fe-arrow-right"></i>
+                                </button>
+                            @endif
 
+                            <!-- Alert -->
+                            <div class="list-alert alert alert-dark alert-dismissible border fade" role="alert">
+                                <div class="row align-items-center">
+                                    <div class="col">
+                                        <div class="form-check">
+                                            <input class="form-check-input" id="listAlertCheckbox" type="checkbox" checked disabled>
+                                            <label class="form-check-label text-white" for="listAlertCheckbox">
+                                                <span class="list-alert-count">0</span> deal(s)
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-auto me-n3">
+                                        <button id="candidate-bulk-delete-btn" data-url="{{ route('candidate.bulkDelete') }}" class="btn btn-sm bg-danger text-white">
+                                            Delete Selected
+                                        </button>
+                                    </div>
+                                </div>
+                                <button type="button" class="list-alert-close btn-close" aria-label="Close"></button>
+                            </div>
+                        </div>
 
-                                                    <!-- Alert -->
-                                                    <div class="list-alert alert alert-dark alert-dismissible border fade" role="alert">
-                                                        <!-- Content -->
-                                                        <div class="row align-items-center">
-                                                            <div class="col">
-                                                                <!-- Checkbox -->
-                                                                <div class="form-check">
-                                                                    <input class="form-check-input" id="listAlertCheckbox" type="checkbox" checked disabled>
-                                                                    <label class="form-check-label text-white" for="listAlertCheckbox">
-                                                                        <span class="list-alert-count">0</span> deal(s)
-                                                                    </label>
-                                                                </div>
-                                                            </div>
-                                                            <div class="col-auto me-n3">
-                                                                <button id="candidate-bulk-delete-btn" data-url="{{ route('candidate.bulkDelete') }}" class="btn btn-sm bg-danger text-white">
-                                                                    Delete Selected
-                                                                </button>
-                                                            </div>
-                                                        </div>
-                                                        <button type="button" class="list-alert-close btn-close" aria-label="Close"></button>
-                                                    </div>
-                                                </div>
 
                     </div>
                 </div>
