bbea8d5e7aabdaadec2492f27fd96582376beed3
Shohbozbek <shohbozbek@Shohbozbeks-iMac.local>
2025-04-08 11:53:26 +0500

    ` "Fayl yo'lini URL ga o'zgartirish qo'shildi"`

--- app/Http/Controllers/DjangoController.php
+++ app/Http/Controllers/DjangoController.php
@@ -9,7 +9,15 @@ class DjangoController extends Controller
     {
         // Django bazasidagi table dan ma’lumot olish
         $data = DB::connection('sqlite_django')->table('student_api_searchrecord')->get();
-        return response()->json($data);
 
+        // Fayl yo‘lini to'liq URL formatiga o'zgartirish
+        $data = $data->map(function ($record) {
+            // Faylning to'liq URL manzilini olish
+            $record->search_image_path = url('uploads/searches/' . basename($record->search_image_path));
+            return $record;
+        });
+
+        return response()->json($data);
     }
 }
+
