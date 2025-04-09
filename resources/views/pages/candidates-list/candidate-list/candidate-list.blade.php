eeb2b062102bf39de28bd185c956cba3f8751eb8
devituz <shohbozbek.uz24@gmail.com>
2025-04-09 20:04:30 +0500

    `student_created_at` o'rniga `search_created_at` foydalanildi.

--- resources/views/pages/candidates-list/candidate-list/candidate-list.blade.php
+++ resources/views/pages/candidates-list/candidate-list/candidate-list.blade.php
@@ -158,7 +158,7 @@
                                             <span class="item-identifier">{{ $student->scan_id ?? 'No scan available' }}</span>
                                         </td>
                                         <td>
-                                            <span class="item-created_at">{{ \Carbon\Carbon::parse($student->student_created_at)->format('M d, Y H:i:s') }}</span>
+                                            <span class="item-created_at">{{ \Carbon\Carbon::parse($student->search_created_at)->format('M d, Y H:i:s') }}</span>
                                         </td>
 
                                     </tr>
