1302e6a062522e384b7edbcac4a9cfc1108d6067
devituz <shohbozbek.uz24@gmail.com>
2025-04-09 20:11:11 +0500

    Vaqt zonasini o'zgartirdi: Tashkent vaqtida ko'rsatish

--- resources/views/pages/candidates-list/candidate-list/candidate-list.blade.php
+++ resources/views/pages/candidates-list/candidate-list/candidate-list.blade.php
@@ -158,7 +158,7 @@
                                             <span class="item-identifier">{{ $student->scan_id ?? 'No scan available' }}</span>
                                         </td>
                                         <td>
-                                            <span class="item-created_at">{{ $student->search_created_at }}</span>
+                                            <span class="item-created_at">{{ \Carbon\Carbon::parse($student->search_created_at)->setTimezone('Asia/Tashkent')->format('M d, Y H:i:s') }}</span>
                                         </td>
 
                                     </tr>
